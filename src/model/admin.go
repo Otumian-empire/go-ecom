80b11a61c11bfc7062c66d70ece58801c22f26fc
otumian <popecan1000@gmail.com>
2024-11-03 18:43:18 +0000

    implement the for admin

---
+++ src/model/admin.go
@@ -0,0 +1,12 @@
+package model
+
+type Admin struct {
+	Id        int64  `db:"id" json:"id"`
+	FullName  string `db:"full_name" json:"fullName"`
+	Email     string `db:"email" json:"email"`
+	Password  string `db:"password" json:"password"`
+	Role      string `db:"role" json:"role"`
+	IsBlocked bool   `db:"is_blocked" json:"isBlocked"`
+	CreatedAt string `db:"created_at" json:"createdAt"`
+	UpdatedAt string `db:"updated_at" json:"updatedAt"`
+}
