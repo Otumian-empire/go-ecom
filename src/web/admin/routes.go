397d687f5c2159c3ef15e81b09b4656241a54f19
otumian <popecan1000@gmail.com>
2024-11-14 20:48:44 +0000

    put the AuthorizeJWT middleware onto the admin routes

--- src/web/admin/routes.go
+++ src/web/admin/routes.go
@@ -2,15 +2,21 @@ package admin
 
 import (
 	"database/sql"
+	"otumian-empire/go-ecom/src/middleware"
+	"otumian-empire/go-ecom/src/model"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Router(router *gin.RouterGroup, db *sql.DB) *gin.RouterGroup {
 	controller := NewController(db)
+	dao := NewDao(db)
+
+	router.POST("/login", controller.Login())
+
+	router.Use(middleware.AuthorizeJWT[model.Admin](dao))
 
 	router.POST("/", controller.CreatedAdmin())
-	router.POST("/login", controller.Login())
 	router.PATCH("/", controller.UpdateProfile())
 	// router.POST("/", controller.UpdateAdminRole())
 	// router.POST("/", controller.ForgetPassword())
